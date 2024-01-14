bd6c7794fc6e486faaa38603bb5c39d916737dea
mustafa <121257754+mustafaakalin@users.noreply.github.com>
2024-01-14 19:08:40 +0300

    canaccesspanel added user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,16 +3,31 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 use Spatie\Permission\Traits\HasRoles;
+use Illuminate\Notifications\Notifiable;
+use Filament\Models\Contracts\FilamentUser;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class User extends Authenticatable
+class User extends Authenticatable  implements FilamentUser
 {
+
+
+
+
     use HasApiTokens, HasFactory, Notifiable, HasRoles;
 
+
+
+    public function canAccessPanel(\Filament\Panel $panel): bool
+    {
+        return true;
+    }
+
+
+
+    
     /**
      * The attributes that are mass assignable.
      *
